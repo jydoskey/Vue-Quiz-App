75ee6705d70b53eb55ea61353b4112f760f70edf
jydoskey <jideomotola33@gmail.com>
2020-02-10 01:59:12 +0100

    Question API added

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Question Box</h1>
+  <div class="question-box-container">
+    <b-jumbotron>
+      
+
+        <template v-slot:lead>
+          {{ currentQuestion.question}}
+        </template>
+
+        <hr class="my-4">
+
+        <p>
+          List of answers
+        </p>
+
+        <b-button variant="primary" href="#">Submit</b-button>
+        <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+</div>
 </template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  mounted() {
+    console.log(this.currentQuestion)
+  },
+}
+</script>
